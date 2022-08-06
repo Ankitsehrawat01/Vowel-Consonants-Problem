e82cf155e295738a6f4b5bf9605de4f98b5e3b19
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-06 18:58:15 +0530

    Alphabets_Problem

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Program to Check Whether an Alphabet is Vowel or Consonant");
+Console.Write("Enter an Alphabet : ");
+char ch = Convert.ToChar(Console.ReadLine().ToLower());
+switch (ch)
+{
+    case 'a':
+        Console.WriteLine("The Alphabet is vowel");
+        break;
+    case 'e':
+        Console.WriteLine("The Alphabet is vowel");
+        break;
+    case 'i':
+        Console.WriteLine("The Alphabet is vowel");
+        break;
+    case 'o':
+        Console.WriteLine("The Alphabet is vowel");
+        break;
+    case 'u':
+        Console.WriteLine("The Alphabet is vowel");
+        break;
+    default:
+        Console.WriteLine("The Alphabet is consonant");
+        break;
+}
